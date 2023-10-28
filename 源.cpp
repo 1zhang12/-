6731bd88e9cf2cdcaa069abbf508f7ebe90e5bcb
1zhang12 <149222358+1zhang12@users.noreply.github.com>
2023-10-28 21:55:07 +0800

    Add files via upload

---
+++ 源.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+void capitalizeFirstLetter(char* sentence) {
+    int length = strlen(sentence);
+    if (length > 0) {
+        sentence[0] = toupper(sentence[0]);
+    }
+    for (int i = 1; i < length; i++) {
+        if (sentence[i - 1] == ' ') {
+            sentence[i] = toupper(sentence[i]);
+        }
+    }
+}
+
+int main() {
+    char sentence[] = "what are yuo diong";
+
+    capitalizeFirstLetter(sentence);
+
+    printf("%s\n", sentence);
+
+    return 0;
+}
